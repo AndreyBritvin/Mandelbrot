09bfa20ab2d42b9ee1b8187dc4d42e389cd4c394
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2025-03-30 22:46:10 +0300

    Working gpu
    Performance not tested yet

--- src/mandelbrot_calc/mandelbrot.cu
+++ src/mandelbrot_calc/mandelbrot.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 
 #include "utils.h"
+#include <stdio.h>
 #include "assert.h"
 #include "config.h"
 #include <omp.h>
@@ -67,58 +68,91 @@ __global__ void fill_pixels_SIMT(int* pixels, double x_center, double y_center, 
     return;
 }*/
 
-__global__ void mandelbrot_kernel(int* pixels, double x_center, double y_center, double scale, int width, int height) {
+__global__ void mandelbrot_kernel(int* pixels, double x_center, double y_center, double scale, int width, int height) 
+{
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int idy = threadIdx.y + blockIdx.y * blockDim.y;
+    
+    double dx = (double) 1 / WIDTH * scale;
+    double dy = (double) 1 / WIDTH * scale;
 
-    if (idx < width && idy < height) {
-        // Реализация вычислений по аналогии с вашей CPU-функцией
-        double x0 = x_center / width - scale / 2 + idx * scale;
-        double y0 = y_center / height - scale / 2 + idy * scale;
+    if (idy >= HEIGHT || idx >= WIDTH)
+    {
+        return;
+    }
+    // double x0 = x_center / height + (idx / height - width  / 2) * scale;
+    // double y0 = y_center / height + (idy / height - height / 2) * scale;
+    double y0 = y_center / HEIGHT - scale / 2 + dy * idy;
+    double x0 = x_center / WIDTH  - scale / 2 + dx * idx;
 
-        int N_count = 0;
-        double X = x0, Y = y0;
-        double R_square_max = 10;
+    int N_count = 0;
+    double X = x0, Y = y0;
+    double R_square_max = 10;
 
-        // Пример вычислений для мандельброта
-        while (N_count < N_EXIT_COUNT) {
-            double X_square = X * X;
-            double Y_square = Y * Y;
-            double XY = X * Y;
-            double R_square = X_square + Y_square;
+    while (N_count < N_EXIT_COUNT) 
+    {
+        double X_square = X * X;
+        double Y_square = Y * Y;
+        double XY = X * Y;
+        double R_square = X_square + Y_square;
 
-            if (R_square > R_square_max) {
-                break;
-            }
-
-            N_count++;
-            X = X_square - Y_square + x0;
-            Y = 2 * XY + y0;
+        if (R_square > R_square_max) 
+        {
+            // printf("Exit ThrX = %3d, ThrY= %3d, BlX = %3d, BlY = %3d N_count = %3d\n"
+            //      "-------begin-------\n"
+            //     "x_screen = %d\n"
+            //     "y_screen = %d\n"
+            //     "X0 = %lf\n"
+            //     "Y0 = %lf\n"
+            //     "X  = %lf\n"
+            //     "Y  = %lf\n"
+            //     "N_count = %d\n"
+            //     "--------end----\n\n",  threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, N_count,
+            //     idx, idy, x0, y0, X, Y, N_count);
+            break;
         }
 
-        // Сохранение результата в массив
-        pixels[idy * width + idx] = generatePixelColor_cuda(N_count);
+        N_count++;
+        X = X_square - Y_square + x0;
+        Y = 2 * XY + y0;
     }
+
+    pixels[idy * width + idx] = generatePixelColor_cuda(N_count);
+    // printf("ThrX = %3d, ThrY= %3d, BlX = %3d, BlY = %3d N_count = %3d\n",  threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, N_count);
+    return;
 }
 
 extern "C"
-__host__ void fill_pixels_SIMT_GPU(int* pixels, double x_center, double y_center, double scale) {
+__host__ void fill_pixels_SIMT_GPU(int* pixels, double x_center, double y_center, double scale) 
+{
     int *d_pixels;
     size_t size = WIDTH * HEIGHT * sizeof(int);
     
     // Выделение памяти на устройстве
-    cudaMalloc((void**)&d_pixels, size);
+    cudaError_t err = cudaMalloc((void**)&d_pixels, size);
+    if (err != cudaSuccess) 
+    {
+        printf("CUDA malloc failed: %s\n", cudaGetErrorString(err));
+    }
+    // printf("CUDA malloc success: %s\n", cudaGetErrorString(err));
 
     // Запуск ядра
-    dim3 blockDim(16, 16);  // Размер блока
+    dim3 blockDim(32, 32);  // Размер блока
     dim3 gridDim((WIDTH + blockDim.x - 1) / blockDim.x, (HEIGHT + blockDim.y - 1) / blockDim.y);  // Размер сетки
 
     mandelbrot_kernel<<<gridDim, blockDim>>>(d_pixels, x_center, y_center, scale, WIDTH, HEIGHT);
-
+    err = cudaGetLastError();
+    if (err != cudaSuccess) 
+    {
+        printf("CUDA kernel launch failed: %s\n", cudaGetErrorString(err));
+    }
     // Копирование данных с устройства на хост
-    cudaMemcpy(pixels, d_pixels, size, cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(pixels, d_pixels, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) 
+    {
+        printf("CUDA memcpy failed: %s\n", cudaGetErrorString(err));
+    }
 
-    // Освобождение памяти
     cudaFree(d_pixels);
     
     return;
